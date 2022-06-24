ebd9f6ee36cd34315609cda384e625a821dc920f
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-06-24 13:01:04 +0200

    Added dark mode support

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useTheme } from 'next-themes';
 
 const Navbar = () => {
+
+  const {theme, setTheme} = useTheme();
+
+
   return (
-    <div>Navbar</div>
+    <div className='container'>
+      <button
+        aria-label="Toggle Dark Mode"
+        type="button"
+        className="bg-teal-300 display-block"
+        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      >Theme</button>
+    </div>
   )
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,14 @@
 import Head from 'next/head'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>Joshua Munganga - Fullstack Developer</title>
         <meta name="description" content="Joshua Munganga - Fullstack Developer frontend focused. Skilled in React.js & Next.js" />
       </Head>
-
+      <Navbar />
       <h1 className="text-3xl font-bold underline">
         Hello World
       </h1>
