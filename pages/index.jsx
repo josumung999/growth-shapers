ca36fb0c0726a8b2943666eee21460147ce2d3d9
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-06-29 12:32:49 +0200

    trying implementing flowbite

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,20 @@
 import Head from 'next/head'
+import Script from 'next/script'
+import { Fragment } from 'react'
 import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Joshua Munganga - Fullstack Developer</title>
-        <meta name="description" content="Joshua Munganga - Fullstack Developer frontend focused. Skilled in React.js & Next.js" />
-      </Head>
-      <Navbar />
-      <h1 className="text-3xl font-bold underline">
-        Hello World
-      </h1>
-    </div>
+      <Fragment>
+        <Head>
+          <title>Joshua Munganga - Fullstack Developer</title>
+          <meta name="description" content="Joshua Munganga - Fullstack Developer frontend focused. Skilled in React.js & Next.js" />
+        </Head>
+        <Navbar />
+        <h1 className="text-3xl font-bold underline">
+          Hello World
+        </h1>
+        {/* {<Script src="../node_modules/flowbite/flowbite.js" />} */}
+      </Fragment>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
+      'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
       extend: {},
@@ -11,6 +12,7 @@ module.exports = {
       }
   },
   plugins: [
-      require('@tailwindcss/typography')
+      require('@tailwindcss/typography'),
+      require('flowbite/plugin')
   ],
 }
