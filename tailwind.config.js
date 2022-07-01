ae1b97b2530b2b98f34d12c00034a69f4320b285
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-07-01 17:36:48 +0200

    Revert "facing several warnings"
    This reverts commit 38699567b8395ff772e5ce8471406ee06a54d9eb.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,12 @@ module.exports = {
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'media',
   theme: {
       extend: {},
       fontFamily: {
           sans: ["'Montserrat'"],
           mono: ["'Inconsolata'"]
-      },
+      }
   },
   plugins: [
       require('@tailwindcss/typography'),
