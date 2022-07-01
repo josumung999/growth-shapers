cadc9e1f34cf11ad7ec8574a8ee2af53de0b3943
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-07-01 13:53:19 +0200

    Configuring i18n in _app.js

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,39 @@
-import '../styles/globals.css'
+import { useRouter } from 'next/router';
 import { ThemeProvider } from 'next-themes';
+import { IntlProvider } from 'react-intl';
+
+import en from '../lang/en.json';
+import fr from '../lang/fr.json';
+
+import '../styles/globals.css';
+
+
+
+// Implementing locales
+const messages = {
+  en,
+  fr,
+}
+
+
+// Getting the direction based on the type of locale
+function getDirection(locale) {
+  if (locale === "ar") {
+    return 'rtl';
+  }
+
+  return "ltr"
+}
 
 function MyApp({ Component, pageProps }) {
+  const { locale } = useRouter();
+
+
   return (
     <ThemeProvider attribute='class'>
-      <Component {...pageProps} />
+      <IntlProvider locale={locale} messages={messages[locale]}>
+        <Component {...pageProps} dir={getDirection(locale)} />
+      </IntlProvider>
     </ThemeProvider>
   )
 }
