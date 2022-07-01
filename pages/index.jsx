6240710eef90bf6dc1886dcee5fb6f3349342641
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-07-01 14:09:08 +0200

    Integrate internationalization successfully

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,31 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { FormattedMessage, useIntl } from 'react-intl';
+
 import Navbar from '../components/Navbar'
 
-export default function Home() {
+export default function Home({ dir }) {
+  const { locales } = useRouter();
+
+  const intl = useIntl();
+
+  const title = intl.formatMessage({ id: "page.home.head.title" });
+  const description = intl.formatMessage({ id: "page.home.meta.description" });
+
   return (
     <div className="container">
       <Head>
-        <title>Joshua Munganga - Fullstack Developer</title>
-        <meta name="description" content="Joshua Munganga - Fullstack Developer frontend focused. Skilled in React.js & Next.js" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
       </Head>
       <Navbar />
       <h1 className="text-3xl font-bold underline">
-        Hello World
+        <FormattedMessage id="page.home.title" />
       </h1>
+      <p>
+        <FormattedMessage id="page.home.description" />
+      </p>
     </div>
   )
 }
