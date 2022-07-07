3d82aa7e9d084ff1b3bd7ded772cf72f4bef2b3b
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-07-07 15:03:45 +0200

    V1-navbar-finished

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   const menuItems = intl.messages['page.menuItems'];
   return (
-      <nav className='sticky rounded top-1 lg:top-3 z-10 mx-4 lg:mx-16 bg-gray-200 backdrop-filter backdrop-blur-lg bg-opacity-30  border-b border-gray-200'>
+      <nav className='sticky rounded top-1 lg:top-3 z-10 mx-2 lg:mx-16 bg-gray-200 backdrop-filter backdrop-blur-lg bg-opacity-30  border-b border-gray-200'>
         <div className="max-w-5xl mx-auto px-4">
           <div className="flex items-center justify-between h-16">
             <span className="text-2xl text-gray-900 font-semibold">Logo</span>
@@ -23,45 +23,59 @@ const Navbar = () => {
                 </a>
               )}
             </div>
-            <div className="flex space-x-4 text-gray-900">
+            <div className="hidden lg:flex space-x-4 text-gray-900">
               <button
                 aria-label="Toggle Dark Mode"
                 type="button"
-                className="w-8 h-8 bg-teal-400 rounded-lg dark:bg-slate-800 flex items-center justify-center hover:ring-2 ring-blue-400 transition-all duration-300 focus:outline-none"
+                className="w-8 h-8 bg-gray-200 rounded-full dark:bg-slate-800 flex items-center justify-center hover:ring-2 ring-blue-400 transition-all duration-300 focus:outline-none"
                 onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
               >
                 {theme === 'light' ? (
-                  <MoonIcon className="text-blue-500 w-5 h-5" />
+                  <MoonIcon className="text-gray-500 w-5 h-5" />
                 ) : (
-                  <SunIcon className="text-blue-400 w-5 h-5" />
+                  <SunIcon className="text-gray-400 w-5 h-5" />
                 )}
               </button>
             </div>
             <div className="lg:hidden">
-              <button
-                aria-label="Open Menu"
-                title="Open Menu"
-                className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
-                onClick={() => setIsMenuOpen(true)}
-              >
-                <svg className="w-5 text-gray-600 dark:text-gray-300" viewBox="0 0 24 24">
-                  <path
-                    fill="currentColor"
-                    d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
-                  />
-                  <path
-                    fill="currentColor"
-                    d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
-                  />
-                  <path
-                    fill="currentColor"
-                    d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
-                  />
-                </svg>
-              </button>
+              <div className="flex items-center justify-center">
+                <button
+                  aria-label="Toggle Dark Mode"
+                  type="button"
+                  className="w-8 h-8 mx-2 bg-gray-200 rounded-full dark:bg-slate-800 flex items-center justify-center hover:ring-2 ring-blue-400 transition-all duration-300 focus:outline-none"
+                  onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+                >
+                  {theme === 'light' ? (
+                    <MoonIcon className="text-gray-500 w-5 h-5" />
+                  ) : (
+                    <SunIcon className="text-gray-400 w-5 h-5" />
+                  )}
+                </button>
+                <button
+                  aria-label="Open Menu"
+                  title="Open Menu"
+                  className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+                  onClick={() => setIsMenuOpen(true)}
+                >
+                  <svg className="w-5 text-gray-600 dark:text-gray-300" viewBox="0 0 24 24">
+                    <path
+                      fill="currentColor"
+                      d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+                    />
+                    <path
+                      fill="currentColor"
+                      d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+                    />
+                    <path
+                      fill="currentColor"
+                      d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+                    />
+                  </svg>
+                </button>
+              </div>
               {isMenuOpen && (
                 <div className="absolute z-50 top-0 left-0 w-full">
-                  <div className="p-5 bg-white dark:bg-slate-600 dark:border-none border rounded shadow-sm">
+                  <div className="p-5 bg-teal-500 dark:bg-teal-700 dark:border-none border rounded shadow-sm">
                     <div className="flex items-center justify-between mb-4">
                       <div>
                         <Link href="/">
@@ -70,7 +84,7 @@ const Navbar = () => {
                             title="Growth Shapers"
                             className="inline-flex items-center"
                           >
-                            <span className="ml-2 text-lg font-bold tracking-wid text-white">
+                            <span className="ml-2 text-2xl font-bold tracking-wid text-slate-900">
                               Growth Shapers
                             </span>
                           </a>
@@ -96,7 +110,7 @@ const Navbar = () => {
                       <ul className="space-y-4">
                         {menuItems.map((item) =>( 
                           <li key={item.id} >
-                            <a className='text-gray-300 text-xl' href={item.link}>
+                            <a className='text-gray-50 dark:text-gray-200 text-xl mx-3' href={item.link}>
                               {item.text}
                             </a>
                           </li>
