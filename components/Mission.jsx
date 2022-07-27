9a2a85b5969a8905ae2f34bcc777e86a716d89bf
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-07-27 14:30:02 +0200

    Added bg color to mission

--- components/Mission.jsx
+++ components/Mission.jsx
@@ -5,7 +5,7 @@ import { FormattedMessage } from 'react-intl'
 
 const Mission = () => {
   return (
-    <div>
+    <div className='bg-white dark:bg-slate-700'>
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-16 lg:py-20">
       <div className="grid gap-10 lg:grid-cols-2">
         <div className="lg:pr-10">
