88704dc088b4b540a0df2c0849cf9e5dd34a36f7
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-07-27 10:45:34 +0200

    Working on mission section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -15,13 +15,12 @@ const Hero = () => {
           </h1>
           <div className='my-8'>
             <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-teal-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                  <FormattedMessage id="page.home.hero.cta1" />
-                  <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-              </a>
-              <a href="#" className="hidden lg:inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                <FormattedMessage id="page.home.hero.cta2" />
-              </a> 
-
+              <FormattedMessage id="page.home.hero.cta1" />
+              <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+            </a>
+            <a href="#" className="hidden lg:inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+              <FormattedMessage id="page.home.hero.cta2" />
+            </a> 
           </div>
         </div>
         <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
