087041e0951fe47d0a448fce36a0ac17e10a1697
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-08-03 13:56:32 +0200

    Short improvements on mobile menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
       <nav className='sticky rounded top-0 z-50 bg-gray-200 backdrop-filter backdrop-blur-lg bg-opacity-30  shadow-md'>
         <div className="max-w-6xl mx-auto px-4">
           <div className="flex items-center justify-between h-16">
-            <span className="text-2xl text-gray-900 dark:text-gray-200 font-black">Growth Shapers</span>
+            <span className="text-2xl text-gray-900 dark:text-gray-200 font-semibold lg:font-bold">Growth Shapers</span>
             <div className="hidden lg:flex space-x-4 text-gray-900">
               {menuItems.map((item) => 
                 <a key={item.id} className="text-gray-700 text-base dark:text-gray-300" href={item.link}>
@@ -84,7 +84,7 @@ const Navbar = () => {
               </div>
               {isMenuOpen && (
                 <div className="absolute z-50 top-0 left-0 w-full">
-                  <div className="p-5 bg-teal-500 dark:bg-teal-700 dark:border-none border rounded shadow-sm">
+                  <div className="p-5 bg-slate-200 dark:bg-slate-700 dark:border-none border rounded shadow-sm">
                     <div className="flex items-center justify-between mb-4">
                       <div>
                         <Link href="/">
@@ -93,7 +93,7 @@ const Navbar = () => {
                             title="Growth Shapers"
                             className="inline-flex items-center"
                           >
-                            <span className="ml-2 text-2xl font-bold tracking-wid text-slate-900">
+                            <span className="ml-2 text-2xl font-semibold tracking-wid text-slate-800 dark:text-slate-300">
                               Growth Shapers
                             </span>
                           </a>
@@ -106,7 +106,7 @@ const Navbar = () => {
                           className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                           onClick={() => setIsMenuOpen(false)}
                         >
-                          <svg className="w-5 text-gray-600 dark:text-gray-300" viewBox="0 0 24 24">
+                          <svg className="w-5 text-gray-800 dark:text-gray-300" viewBox="0 0 24 24">
                             <path
                               fill="currentColor"
                               d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -119,7 +119,7 @@ const Navbar = () => {
                       <ul className="space-y-4">
                         {menuItems.map((item) =>( 
                           <li key={item.id} >
-                            <a className='text-gray-50 dark:text-gray-200 text-xl mx-3' href={item.link}>
+                            <a className='text-slate-800 dark:text-gray-200 text-xl mx-3' href={item.link}>
                               {item.text}
                             </a>
                           </li>
