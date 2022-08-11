9904741a794a1103dea3a443fbbc24ac5dc755af
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-08-11 15:03:32 +0200

    animation toujours

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -33,7 +33,7 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     AOS.init({
       easing: "ease-out-cubic",
-      once : true,
+      once : false,
       offset: 50
     });
   }, []);
