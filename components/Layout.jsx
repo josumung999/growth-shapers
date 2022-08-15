b075b2b3e0628dcb9f665c97e041402b0c819e6c
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-08-15 10:56:39 +0200

    added the layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Head from 'next/head'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
-const Layout = () => {
+const Layout = ({title, description, children}) => {
   return (
-    <div>Layout</div>
+    <div>
+      <Head>
+        <title>
+          {title ? `${title} - Growth Shapers` : 'Growth Shapers'}
+          {description && <meta name='description' content={description} />}
+        </title>
+      </Head>
+      <Navbar />
+      <main>
+        {children}
+      </main>
+      <Footer />
+    </div>
   )
 }
 
