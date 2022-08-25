02a6af7bd80d4302926054a1d921756a0f249004
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-08-25 19:14:57 +0200

    still working on that shitty form

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -16,26 +16,32 @@ const Contact = () => {
             <h2 className='text-2xl font-semibold'>Let&apos;s work together</h2>
             <form action="" method="post">
                 <div className="md:flex items-center mt-12">
+                    <div className="w-full flex flex-col">
+                        <label className="font-semibold leading-none text-gray-800 dark:text-gray-300">Name</label>
+                        <input type="text" className="leading-none dark:text-gray-50 text-gray-700 p-3 focus:outline-none mt-4 border border-gray-300 focus:border-teal-500 dark:border-0 bg-white dark:bg-gray-800 rounded"/>
+                    </div>     
+                </div>
+                <div className="md:flex items-center mt-8">
                     <div className="w-full md:w-1/2 flex flex-col">
-                        <label className="font-semibold leading-none text-gray-300">Name</label>
-                        <input type="text" className="leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded" />
+                        <label className="font-semibold leading-none text-gray-800 dark:text-gray-300">Email</label>
+                        <input type="text" className="leading-none dark:text-gray-50 text-gray-700 p-3 focus:outline-none mt-4 border border-gray-300 focus:border-teal-500 dark:border-0 bg-white dark:bg-gray-800 rounded" />
                     </div>
                     <div className="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
-                        <label className="font-semibold leading-none text-gray-300">Phone</label>
-                        <input type="email" className="leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded"/>
+                        <label className="font-semibold leading-none text-gray-800 dark:text-gray-300">Phone</label>
+                        <input type="email" className="leading-none dark:text-gray-50 text-gray-700 p-3 focus:outline-none mt-4 border border-gray-300 focus:border-teal-500 dark:border-0 bg-white dark:bg-gray-800 rounded"/>
                     </div>
                 </div>
                 <div className="md:flex items-center mt-8">
                     <div className="w-full flex flex-col">
-                        <label className="font-semibold leading-none text-gray-300">Subject</label>
-                        <input type="text" className="leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded"/>
+                        <label className="font-semibold leading-none text-gray-800 dark:text-gray-300">Subject</label>
+                        <input type="text" className="leading-none dark:text-gray-50 text-gray-700 p-3 focus:outline-none mt-4 border border-gray-300 focus:border-teal-500 dark:border-0 bg-white dark:bg-gray-800 rounded"/>
                     </div>
-                    
                 </div>
+                
                 <div>
                     <div className="w-full flex flex-col mt-8">
-                        <label className="font-semibold leading-none text-gray-300">Message</label>
-                        <textarea type="text" className="h-40 text-base leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-800 border-0 rounded"></textarea>
+                        <label className="font-semibold leading-none text-gray-800 dark:text-gray-300">Message</label>
+                        <textarea type="text" className="h-40 text-base leading-none dark:text-gray-50 text-gray-700 p-3 focus:outline-none mt-4 border border-gray-300 focus:border-teal-500 dark:border-0 bg-white dark:bg-gray-800 rounded"></textarea>
                     </div>
                 </div>
                 <div className="flex items-center justify-center w-full">
