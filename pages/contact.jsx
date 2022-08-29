eb2096dc2d335774f15be8f3ed5b7bb61fb3a183
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-08-29 20:20:44 +0200

    finished translating the contact page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useIntl } from 'react-intl';
+import { useIntl, FormattedMessage } from 'react-intl';
 import Layout from '../components/Layout';
 
 const Contact = () => {
@@ -13,34 +13,46 @@ const Contact = () => {
       <div className='-mt-20 bg-white dark:bg-slate-900'>
         <div className="flex flex-row px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-16 lg:px-10 lg:py-20">
           <div className="basis-3/5 h-full lg:py-28">
-            <h2 className='text-2xl font-semibold'>Let&apos;s work together</h2>
+            <h2 className='text-2xl font-semibold'>
+              <FormattedMessage id="page.contact.form.title" />
+            </h2>
             <form action="" method="post">
                 <div className="md:flex items-center mt-12">
                     <div className="w-full flex flex-col">
-                        <label className="font-semibold leading-none text-gray-800 dark:text-gray-300">Name</label>
+                        <label className="font-semibold leading-none text-gray-800 dark:text-gray-300">
+                          <FormattedMessage id="page.contact.form.label.name" />
+                        </label>
                         <input type="text" className="leading-none dark:text-gray-50 text-gray-700 p-3 focus:outline-none mt-4 border border-gray-300 focus:border-teal-500 dark:border-0 bg-white dark:bg-gray-800 rounded"/>
                     </div>     
                 </div>
                 <div className="md:flex items-center mt-8">
                     <div className="w-full md:w-1/2 flex flex-col">
-                        <label className="font-semibold leading-none text-gray-800 dark:text-gray-300">Email</label>
+                        <label className="font-semibold leading-none text-gray-800 dark:text-gray-300">
+                        <FormattedMessage id="page.contact.form.label.email" />
+                        </label>
                         <input type="text" className="leading-none dark:text-gray-50 text-gray-700 p-3 focus:outline-none mt-4 border border-gray-300 focus:border-teal-500 dark:border-0 bg-white dark:bg-gray-800 rounded" />
                     </div>
                     <div className="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
-                        <label className="font-semibold leading-none text-gray-800 dark:text-gray-300">Phone</label>
+                        <label className="font-semibold leading-none text-gray-800 dark:text-gray-300">
+                          <FormattedMessage id="page.contact.form.label.phone" />
+                        </label>
                         <input type="email" className="leading-none dark:text-gray-50 text-gray-700 p-3 focus:outline-none mt-4 border border-gray-300 focus:border-teal-500 dark:border-0 bg-white dark:bg-gray-800 rounded"/>
                     </div>
                 </div>
                 <div className="md:flex items-center mt-8">
                     <div className="w-full flex flex-col">
-                        <label className="font-semibold leading-none text-gray-800 dark:text-gray-300">Subject</label>
+                        <label className="font-semibold leading-none text-gray-800 dark:text-gray-300">
+                          <FormattedMessage id="page.contact.form.label.subject" />
+                        </label>
                         <input type="text" className="leading-none dark:text-gray-50 text-gray-700 p-3 focus:outline-none mt-4 border border-gray-300 focus:border-teal-500 dark:border-0 bg-white dark:bg-gray-800 rounded"/>
                     </div>
                 </div>
                 
                 <div>
                     <div className="w-full flex flex-col mt-8">
-                        <label className="font-semibold leading-none text-gray-800 dark:text-gray-300">Message</label>
+                        <label className="font-semibold leading-none text-gray-800 dark:text-gray-300">
+                          <FormattedMessage id="page.contact.form.label.message" />
+                        </label>
                         <textarea type="text" className="h-40 text-base leading-none dark:text-gray-50 text-gray-700 p-3 focus:outline-none mt-4 border border-gray-300 focus:border-teal-500 dark:border-0 bg-white dark:bg-gray-800 rounded"></textarea>
                     </div>
                 </div>
@@ -62,7 +74,7 @@ const Contact = () => {
                         text-white
                         my-12
                         ">
-                        Send message
+                        <FormattedMessage id="page.contact.form.button.submit" />
                     </button>
                 </div>
             </form>
