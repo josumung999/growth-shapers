1d1b461f223ad200ca00acf71095f9088da19e3d
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-08-29 21:44:36 +0200

    still working on contact form

---
+++ pages/api/contact.js
@@ -0,0 +1,35 @@
+import nodemailer from 'nodemailer';
+
+const sendMail = (req, res) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.TEST_EMAIL,
+      pass: process.env.TEST_PASS
+    },
+    secure: true
+  });
+
+  const mailData = {
+    from: process.env.TEST_EMAIL,
+    to: 'jmunganga039@gmail.com',
+    subject: `Message From ${req.body.name}`,
+    text: `<h3>${req.body.subject}</h3><p><b>Message Content: </b>${req.body.message}</p><p>Sender: ${req.body.name} <br> Email: ${req.body.email} <br> Phone number: ${req.body.phone} </p>`,
+    html: `<h3>${req.body.subject}</h3><p><b>Message Content: </b>${req.body.message}</p><p>Sender: ${req.body.name} <br> Email: ${req.body.email} <br> Phone number: ${req.body.phone} </p>`
+   };
+
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(info);
+    }
+   })
+
+   res.status(200).send(
+    'Message sent successfully, thanks for contacting US'
+   );
+}
+
+export default sendMail;
