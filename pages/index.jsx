9e9d1e4ed8e518d9808ec988950ba8482a5b7a5f
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-11-03 12:57:41 +0100

    removed unused values

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,12 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { Fragment } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import Action from '../components/Action';
-import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 import HowItWorks from '../components/HowItWorks';
 import Layout from '../components/Layout';
 import Mission from '../components/Mission';
-
-import Navbar from '../components/Navbar'
 import Solutions from '../components/Solutions';
 
 export default function Home({ dir }) {
